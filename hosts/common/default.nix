c5439a757182098020596aeb3b8625ab0b0e5005
Martin Claus <martinclaus@posteo.de>
2025-07-06 15:59:37 +0200

    remove commas

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -5,8 +5,8 @@
     sshKeys = lib.mkOption {
       type = lib.types.listOf lib.types.str;
       default = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICzXbYP1RH7+Lqlx65uzIyLe7XtoIlfqE+C9rvP0tqNt",
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICX6mqgg8gx2PIODrvKl5sOR+EW4mDM5w3zREn1vDYUW",
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICzXbYP1RH7+Lqlx65uzIyLe7XtoIlfqE+C9rvP0tqNt"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICX6mqgg8gx2PIODrvKl5sOR+EW4mDM5w3zREn1vDYUW"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIELaMlG7yEFF+959jfYlGQd2K2mpxVLX6sWHj/ITWOB+"
       ];
       description = "SSH keys for the system";
